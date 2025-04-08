7a8f090c9862bc178ca7eecf326850c860d97522
Robert Stolz <robert.stolz87@gmail.com>
2025-04-08 13:28:44 +0200

    first setup

---
+++ frontend/src/components/Welcome.tsx
@@ -0,0 +1,9 @@
+export default function Welcome(){
+    return (
+        <div className="welcome-container">
+            <h1>Welcome to the Sudoku Animal Hub!</h1>
+            <p>Click the button below to start playing.</p>
+            <button>Start Game</button>
+        </div>
+    );
+}
