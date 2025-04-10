da1a124c1f83630518815a175fee0d4030a9f777
Robert Stolz <robert.stolz87@gmail.com>
2025-04-10 16:32:00 +0200

    npm and some routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Welcome from "./components/Welcome.tsx";
+import Navbar from "./components/Navbar.tsx";
+import Footer from "./components/Footer.tsx";
 
 export default function App() {
 
 
   return (
     <>
+        <Navbar/>
         <Welcome/>
+        <Footer/>
     </>
   )
 }
