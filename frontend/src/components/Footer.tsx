3d54e6fd631537e1cc5348f7e4c3a5f4e9271adb
Robert Stolz <robert.stolz87@gmail.com>
2025-04-11 10:15:24 +0200

    Logo Start

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,10 +3,6 @@ export default function Footer() {
     return (
         <footer className="footer">
             <p>Sudoku Animal Hub © 2025 by R.Stolz</p>
-            <p>
-                Bild: <a href="https://www.vecteezy.com/vector-art/9377494-sudoku-word-logo-with-toucan-bird" target="_blank" rel="noopener noreferrer">
-                Sudoku Logo Design with Pencil</a> von <a href="https://www.vecteezy.com" target="_blank" rel="noopener noreferrer">Vecteezy</a>
-            </p>
         </footer>
-    );
+    )
 }
--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -1,9 +1,35 @@
+import welcomePic from "../assets/Sudoku-Logo.jpg"
+import "./styles/Welcome.css"
+import {useState} from "react";
+
 export default function Welcome(){
+
+    const [showCredits, setShowCredits] = useState(false);
+
+    const toggleCredits = () => {
+        setShowCredits(prev => !prev);
+    };
+
     return (
-        <div className="welcome-container">
-            <h1>Welcome to the Sudoku Animal Hub!</h1>
-            <p>Click the button below to start playing.</p>
-            <button>Start Game</button>
+        <div>
+            <h2>Welcome to Sudoku Animal Hub</h2>
+            <p>Click on the Picture or the Play button to start playing!</p>
+
+            <div className="image-wrapper">
+                <img
+                    src={welcomePic}
+                    alt="Welcome to RevealHub"
+                    className="logo-welcome"
+                />
+                <div className="info-icon" onClick={toggleCredits} title="Bildnachweis">i</div>
+
+                {showCredits && (
+                    <div className="image-credit">
+                        Bild: <a href="https://www.vecteezy.com/vector-art/9377494-sudoku-word-logo-with-toucan-bird" target="_blank" rel="noopener noreferrer">
+                        Sudoku word logo</a> von <a href="https://www.vecteezy.com" target="_blank" rel="noopener noreferrer">Vecteezy</a>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
