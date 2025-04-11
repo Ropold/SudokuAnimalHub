d9ead1e9185e62eb6e61f0c013cd51a1f1a1e4d2
Robert Stolz <robert.stolz87@gmail.com>
2025-04-11 14:43:16 +0200

    all animal pics

--- frontend/src/components/utils/AnimalEnumImages.ts
+++ frontend/src/components/utils/AnimalEnumImages.ts
@@ -1,5 +1,50 @@
+import anteaterPic from "../../assets/animalEnumImages/anteater.jpg";
+import armadilloPic from "../../assets/animalEnumImages/armadillo.jpg";
+import bearPic from "../../assets/animalEnumImages/bear.jpg";
+import beaverPic from "../../assets/animalEnumImages/beaver.jpg";
+import birdPic from "../../assets/animalEnumImages/bird.jpg";
 import catPic from "../../assets/animalEnumImages/cat.jpg";
-import {AnimalEnum} from "../model/AnimalEnum.ts";
+import chameleonPic from "../../assets/animalEnumImages/chameleon.jpg";
+import chickenPic from "../../assets/animalEnumImages/chicken.jpg";
+import cowPic from "../../assets/animalEnumImages/cow.jpg";
+import crocodilePic from "../../assets/animalEnumImages/crocodile.jpg";
+import deerPic from "../../assets/animalEnumImages/deer.jpg";
+import dogPic from "../../assets/animalEnumImages/dog.jpg";
+import dolphinPic from "../../assets/animalEnumImages/dolphin.jpg";
+import duckPic from "../../assets/animalEnumImages/duck.jpg";
+import eaglePic from "../../assets/animalEnumImages/eagle.jpg";
+import elephantPic from "../../assets/animalEnumImages/elephant.jpg";
+import fishPic from "../../assets/animalEnumImages/fish.jpg";
+import foxPic from "../../assets/animalEnumImages/fox.jpg";
+import frogPic from "../../assets/animalEnumImages/frog.jpg";
+import giraffePic from "../../assets/animalEnumImages/giraffe.jpg";
+import hedgehogPic from "../../assets/animalEnumImages/hedgehog.jpg";
+import hippopotamusPic from "../../assets/animalEnumImages/hippopotamus.jpg";
+import horsePic from "../../assets/animalEnumImages/horse.jpg";
+import kangarooPic from "../../assets/animalEnumImages/kangaroo.jpg";
+import koalaPic from "../../assets/animalEnumImages/koala.jpg";
+import llamaPic from "../../assets/animalEnumImages/llama.jpg";
+import lionPic from "../../assets/animalEnumImages/lion.jpg";
+import monkeyPic from "../../assets/animalEnumImages/monkey.jpg";
+import mousePic from "../../assets/animalEnumImages/mouse.jpg";
+import octopusPic from "../../assets/animalEnumImages/octopus.jpg";
+import owlPic from "../../assets/animalEnumImages/owl.jpg";
+import parrotPic from "../../assets/animalEnumImages/parrot.jpg";
+import penguinPic from "../../assets/animalEnumImages/penguin.jpg";
+import pigPic from "../../assets/animalEnumImages/pig.jpg";
+import raccoonPic from "../../assets/animalEnumImages/raccoon.jpg";
+import rabbitPic from "../../assets/animalEnumImages/rabbit.jpg";
+import sheepPic from "../../assets/animalEnumImages/sheep.jpg";
+import skunkPic from "../../assets/animalEnumImages/skunk.jpg";
+import snakePic from "../../assets/animalEnumImages/snake.jpg";
+import squirrelPic from "../../assets/animalEnumImages/squirrel.jpg";
+import tigerPic from "../../assets/animalEnumImages/tiger.jpg";
+import turtlePic from "../../assets/animalEnumImages/turtle.jpg";
+import whalePic from "../../assets/animalEnumImages/whale.jpg";
+import wolfPic from "../../assets/animalEnumImages/wolf.jpg";
+import zebraPic from "../../assets/animalEnumImages/zebra.jpg";
+
+import { AnimalEnum } from "../model/AnimalEnum.ts";
 
 export const animalsEnumImages: Record<AnimalEnum, string> = {
     ANTEATER: anteaterPic,
@@ -47,4 +92,4 @@ export const animalsEnumImages: Record<AnimalEnum, string> = {
     WHALE: whalePic,
     WOLF: wolfPic,
     ZEBRA: zebraPic,
-}
+};
