37cf1918a13b46bb25cde9429a52204525fcf1cf
Robert Stolz <robert.stolz87@gmail.com>
2025-04-16 16:26:36 +0200

    details button favorite

--- frontend/src/components/Details.tsx
+++ frontend/src/components/Details.tsx
@@ -5,7 +5,13 @@ import { AnimalModel, DefaultAnimal } from "./model/AnimalModel.ts";
 import { DefaultUserDetails, UserDetails } from "./model/UserDetailsModel.ts";
 import "./styles/Profile.css"
 
-export default function Details() {
+type DetailsProps = {
+    user: string;
+    favorites: string[];
+    toggleFavorite: (animalId: string) => void;
+}
+
+export default function Details(props: Readonly<DetailsProps>) {
     const [animal, setAnimal] = useState<AnimalModel>(DefaultAnimal);
     const [githubUser, setGithubUser] = useState<UserDetails>(DefaultUserDetails);
     const { id } = useParams<{ id: string }>();
@@ -33,14 +39,27 @@ export default function Details() {
         }
     }, [animal.githubId]);
 
+    const isFavorite = props.favorites.includes(animal.id);
+
     return (
         <>
             <div>
                 <h2>{animal.name}</h2>
                 <p><strong>Name:</strong> {animal.name}</p>
                 <p><strong>Category:</strong> {animal.animalEnum}</p>
-                <p><strong>Description:</strong> {animal.description}</p>
+                <p><strong>Description:</strong> {animal.description || "No description available"}</p>
                 <img src={animal.imageUrl} alt={animal.name} style={{ maxWidth: "300px" }} />
+                {props.user !== "anonymousUser" && (
+                    <div>
+                        <button
+                            className={`button-group-button ${isFavorite ? "favorite-on" : "favorite-off"}`}
+                            onClick={() => props.toggleFavorite(animal.id)}
+                        >
+                            ♥
+                        </button>
+                    </div>
+
+                )}
             </div>
             <div>
                 <h3>Added by User</h3>
