4bffc1e15c9e1602ebaca38242ffedebfa28be13
Robert Stolz <robert.stolz87@gmail.com>
2025-04-25 14:58:45 +0200

    buttons preview

--- frontend/src/components/Play.tsx
+++ frontend/src/components/Play.tsx
@@ -21,6 +21,7 @@ export default function Play(props: Readonly<PlayProps>) {
     const [showPreviewMode, setShowPreviewMode] = useState<boolean>(true);
     const [gameFinished, setGameFinished] = useState<boolean>(true);
     const [deckEnum, setDeckEnum] = useState<DeckEnum>("TEMP_DECK");
+    const [difficultyEnum, setDifficultyEnum] = useState<string>("EASY");
     const [time, setTime] = useState<number>(0);
     const [intervalId, setIntervalId] = useState<number | null>(null);
     const [showNameInput, setShowNameInput] = useState<boolean>(false);
@@ -42,8 +43,23 @@ export default function Play(props: Readonly<PlayProps>) {
 
     return(
         <>
-        <h3>Play</h3>
-        <p>{props.user}</p>
+            {showPreviewMode && (
+                <div>
+                    <div className="space-between">
+                    <h4>Choose a deck:</h4>
+                        <button onClick={() => setDeckEnum("TEMP_DECK")} className={`button-group-button ${deckEnum === "TEMP_DECK" ? "active-button-deck-difficulty" : ""}`}>Temp Deck</button>
+                        <button onClick={() => setDeckEnum("SAVED_DECK")} className={`button-group-button ${deckEnum === "SAVED_DECK" ? "active-button-deck-difficulty" : ""}`}>Saved Deck</button>
+                        <button onClick={() => setDeckEnum("NUMBER_DECK")} className={`button-group-button ${deckEnum === "NUMBER_DECK" ? "active-button-deck-difficulty" : ""}`}>Number Deck</button>
+                    </div>
+                    <div className="space-difficulty">
+                    <h4>Choose a difficulty:</h4>
+                        <button onClick={() => setDifficultyEnum("EASY")} className={`button-group-button ${difficultyEnum === "EASY" ? "active-button-deck-difficulty" : ""}`}>Easy</button>
+                        <button onClick={() => setDifficultyEnum("MEDIUM")} className={`button-group-button ${difficultyEnum === "MEDIUM" ? "active-button-deck-difficulty" : ""}`}>Medium</button>
+                        <button onClick={() => setDifficultyEnum("HARD")} className={`button-group-button ${difficultyEnum === "HARD" ? "active-button-deck-difficulty" : ""}`}>Hard</button>
+                    </div>
+                </div>
+            )}
         </>
+
     )
 }
